1dd6a549680a4a4486676c74b8a6f865d4ec14a9
Mohit <mohja@microsoft.com>
2020-12-22 16:23:56 +0530

    first push apr 2020 website

---
+++ js/publications.js
@@ -0,0 +1,15 @@
+$('#checkboxes-j').click(function() {
+  $(".journal").toggle(this.checked);
+});
+
+$('#checkboxes-c').click(function() {
+  $(".conference").toggle(this.checked);
+});
+
+$('#checkboxes-p').click(function() {
+  $(".patent").toggle(this.checked);
+});
+
+$('#checkboxes-w').click(function() {
+  $(".workshop").toggle(this.checked);
+});
